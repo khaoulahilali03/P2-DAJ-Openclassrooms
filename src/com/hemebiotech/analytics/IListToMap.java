9faf7bcd4f47d6face40c4aa934255ed743f3b7b
HILALI Khaoula <hilali.khaoula@gmail.com>
2022-05-25 13:29:32 +0200

    Add Javadoc to the classes and interface ListToMap

--- src/com/hemebiotech/analytics/IListToMap.java
+++ src/com/hemebiotech/analytics/IListToMap.java
@@ -1,8 +1,18 @@
 package com.hemebiotech.analytics;
 
 import java.util.Map;
-
+/**
+ * 
+ * The return value from the operation, which is a conversion of a list to a map of strings as a key and number of iterations as a value,
+ * No duplications
+ * 
+ * The implementation need to order the map
+ * 
+ */
 public interface IListToMap {
-	
+	/*
+	 * @return a sorted map of each symptom and his number 
+	 * of iteration from the retrieved list
+	 */
 	public Map<String,Integer> symptomsMap(); 
 }
--- src/com/hemebiotech/analytics/ListToMap.java
+++ src/com/hemebiotech/analytics/ListToMap.java
@@ -7,12 +7,23 @@ import java.util.TreeMap;
 
 public class ListToMap implements IListToMap{
 	
+	/*
+	 * @param The list of symptoms
+	 */
 	private List <String> symptomsList;
 	
+	/*
+	 * Constructor of ListToMap
+	 */
 	public  ListToMap(List<String>symptomsList) {
 		symptomsList = this.symptomsList;
 	}
 	
+	/*
+	 * Method symptomsMap() allows to convert the list of symptoms to a sorted map 
+	 * with the number of iterations for each symptom 
+	 */
+	@Override
 	public Map<String,Integer> symptomsMap(){
 		ReadSymptomDataFromFile symptoms = new ReadSymptomDataFromFile ("symptoms.txt");
 		ArrayList <String> listSymp = new ArrayList<>();
